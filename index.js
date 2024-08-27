2faa51a78a1bd99ba1f25738dfecd597ffc1b078
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-08-27 22:41:27 +0100

    Update complete code

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 let bobData = {
-    name: "bob wesley",
-    age: 34,
-    dob: "January 1st, 1990",
-    hasFriends: false,
-    viableSocialLife: false,
-    get length () {
-        return Object.keys(this).length;
-    }
+  name: "bob wesley",
+  age: 34,
+  dob: "January 1st, 1990",
+  hasFriends: false,
+  viableSocialLife: false,
+  get length() {
+    return Object.keys(this).length;
+  },
 };
 
 console.log(bobData.length);
-console.log(bobData["name"])
-console.log(bobData.name)
+console.log(bobData["name"]);
+console.log(bobData.name);
 
-let wesleyFamily=[
-    "Mr. wesley",
-    "Mrs. wesley",
-    "Bob wesley",
-    "Charlotte wesley"
-]
-console.log(wesleyFamily.length)
-console.log(wesleyFamily[2])
-console.log(wesleyFamily['2'])
-wesleyFamily.pop();
-wesleyFamily.push("Bob's uncle")
+let wesleyFamily = [
+  "Mr. wesley",
+  "Mrs. wesley",
+  "Bob wesley",
+  "Charlotte wesley",
+];
+// console.log(wesleyFamily.0)
+// console.log(wesleyFamily.'0')
+console.log(wesleyFamily.length);
+console.log(wesleyFamily[2]);
+console.log(wesleyFamily["2"]);
+wesleyFamily;
+wesleyFamily.pop(); // removes last item
+wesleyFamily;
+wesleyFamily.push("Bob's uncle"); // adds new item to last position
+wesleyFamily;
