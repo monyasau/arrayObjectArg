598aed20abc8ded5c0859b69b22c2d62daff4f6f
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-08-27 21:48:37 +0100

    complete code

---
+++ index.js
@@ -0,0 +1,26 @@
+let bobData = {
+    name: "bob wesley",
+    age: 34,
+    dob: "January 1st, 1990",
+    hasFriends: false,
+    viableSocialLife: false,
+    get length () {
+        return Object.keys(this).length;
+    }
+};
+
+console.log(bobData.length);
+console.log(bobData["name"])
+console.log(bobData.name)
+
+let wesleyFamily=[
+    "Mr. wesley",
+    "Mrs. wesley",
+    "Bob wesley",
+    "Charlotte wesley"
+]
+console.log(wesleyFamily.length)
+console.log(wesleyFamily[2])
+console.log(wesleyFamily['2'])
+wesleyFamily.pop();
+wesleyFamily.push("Bob's uncle")
